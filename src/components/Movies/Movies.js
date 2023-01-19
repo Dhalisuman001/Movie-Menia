eebd1d42c5869342a5571cedfc01934dcab29df9
Suman Dhali <dhalisuman001@gmail.com>
2023-01-19 11:26:37 +0530

    Movies, MovieList, Movie component done

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,20 +1,41 @@
 import React from "react";
-// import {
-//   Box,
-//   CircularProgress,
-//   useMediaQuery,
-//   Typography,
-// } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 // import { useSelector } from "react-redux";
 // import { MovieList, Pagination, FeaturedMovie } from "../index";
 
 import { useGetMoviesQuery } from "../../redux/Api/TMDB";
+import MovieList from "../MovieList/MovieList";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  return <div>Movies</div>;
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies found with that name.
+          <br />
+          Please searh for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return "An error has occured.";
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
