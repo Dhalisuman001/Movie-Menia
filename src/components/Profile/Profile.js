820ec4eaf1a630504cb717935e81acdbae1dd067
Suman Dhali <dhalisuman001@gmail.com>
2023-01-20 20:25:55 +0530

    Profile section added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Avatar, Box, Button, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import useStyles from "./styles";
+import { Navigate } from "react-router";
 
-const Profile = () => {
-  return <div>Profile</div>;
-};
+function Profile() {
+  const { user } = useSelector((state) => state.userAuth);
+  const style = useStyles();
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("userInfo");
+    return <Navigate to={"/"} />;
+  };
+
+  return (
+    <Box className={style.container}>
+      <Avatar
+        alt="Remy Sharp"
+        src={user?.photoURL}
+        sx={{ width: 150, height: 150 }}
+      />
+      <Typography fontSize={25} fontWeight="bolder">
+        {user?.displayName}
+      </Typography>
+      <Typography fontSize={10} fontWeight="bold">
+        Mail: {user?.email}
+      </Typography>
+      <span style={{ marginTop: "20px" }}>
+        <Button variant="contained" onClick={logout}>
+          Logout
+        </Button>
+      </span>
+    </Box>
+  );
+}
 
 export default Profile;
---
+++ src/components/Profile/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  // button: {
+  //   backgroundColor: "black",
+  // },
+}));
